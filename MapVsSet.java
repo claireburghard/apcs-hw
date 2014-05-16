800a62a1002b253c2bf75379d7532819f071409a
Claire Burghard <claireburghard@gmail.com>
2014-05-16 10:11:49 -0400

    Incomplete Homework 19

---
+++ MapVsSet.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class MapVsSet {
+
+    public static void main (String[] args) {
+
+	Hashmap map = new Hashmap();
+	Hashset set = new Hashset();
+	long start,elapsed;
+
+	//Inserting values into the map and set; no duplicate elements
+	System.out.println("Inserting non-duplicate elements\n");
+	
+	int inserts = 1000;
+	
+	start = System.currentTimeMillis();
+	for (int i = 0; i < inserts; i++){
+	    map.put(i,i);
+	}
+	elapsed = System.currentTimeMillis()-start;
+	System.out.println("Inserting " + inserts + " values into the map:");
+	System.out.println("Time: "+elapsed+"\n");
+
+	start = System.currentTimeMillis();
+	for (int i = 0; i < inserts; i++){
+	    set.add(i);
+	}
+	elapsed = System.currentTimeMillis()-start;
+	System.out.println("Inserting " + inserts + " values into the set:");
+	System.out.println("Time: "+elapsed+"\n");
+
+	//Inserting values into the map and set; duplicate elements
+	System.out.println("Inserting elements with duplicates");
+	Random r = new Random();
+
+	start = System.currentTimeMillis();
+	for (int i = 0; i < inserts; i++){
+	    rand = r.nextInt(inserts/2);
+	    map.put(rand,rand);
+	}
+	elapsed = System.currentTimeMillis()-start;
+	System.out.println("Inserting " + inserts + " values into the map:");
+	System.out.println("Time: "+elapsed+"\n");
+
+	start = System.currentTimeMillis();
+	for (int i = 0; i < inserts; i++){
+	    rand = r.nextInt(inserts/2);
+	    set.add(rand);
+	}
+	elapsed = System.currentTimeMillis()-start;
+	System.out.println("Inserting " + inserts + " values into the set:");
+	System.out.println("Time: "+elapsed+"\n");
+
+    }
+
+}
+
+
+
+	
+
+	
